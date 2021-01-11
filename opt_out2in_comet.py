496758d5e04e5d031ec640ce6a2354f48511ef2d
Lexie McIsaac <amcisaac@comet-ln3.sdsc.edu>
2021-01-11 14:37:56 -0800

    consolidated comet opt script

--- opt_out2in_comet.py
+++ opt_out2in_comet.py
@@ -1,20 +1,17 @@
 import sys
 import numpy as np
 from qchem_helper import *
-import timeit
-
 
 output = sys.argv[1]
 
 with open(output,'r') as outfile:
     lines = outfile.readlines()
 
-
 last_geom = get_geom_e_opt_last(lines)
 atom_names = last_geom[:,0]
 atom_xyz = last_geom[:,1:].astype(float)
 rem,spcharge=get_rem_sp(lines)
-
+#print(rem)
 input_prefix='.'.join(output.split('.')[0:-1])
 n_opt = int(input_prefix.split('_')[-1])
 new_input_name = '_'.join(input_prefix.split('_')[0:-1]) +'_'+ str(n_opt+1) + '.in'
